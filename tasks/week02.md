590ad694d6e2616f935f1d025c91b9e7a5a09b42
Radovan Haluska <radovan.haluska1@gmail.com>
2025-10-08 10:25:30 +0200

    Publish homework for Week 2

---
+++ tasks/week02.md
@@ -0,0 +1,19 @@
+# Week 2 Homework
+
+## 1) Best Response Value Function
+- Plot the best response value function for a 2xN zero-sum game
+
+## 2) Support Enumeration
+- Given a pair of supports, construct a system of linear equations and check whether there exists a Nash equilibrium for those supports
+- Implement the Support Enumeration algorithm which enumerates all possible pairs of supports to find all Nash equilibria in a normal-form game
+
+## 3) Nash Equilibrium == Maximin Strategies in Zero-Sum Games
+- Prove the two implications from the lecture slides
+
+# Week 2 Template
+
+See `templates/week02.py` for function definitions and docstrings describing expected inputs and outputs.
+
+# Week 2 Tests
+
+Execute `pytest tests/test_week02.py` to test your solutions against our reference implementations.
---
+++ templates/week02.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def plot_best_response_value_function(row_matrix: np.ndarray, step_size: float) -> None:
+    """Plot the best response value function for the row player in a 2xN zero-sum game.
+
+    Parameters
+    ----------
+    row_matrix : np.ndarray
+        The row player's payoff matrix
+    step_size : float
+        The step size for the probability of the first action of the row player
+    """
+
+    raise NotImplementedError
+
+
+def verify_support(
+    matrix: np.ndarray, row_support: np.ndarray, col_support: np.ndarray
+) -> np.ndarray | None:
+    """Construct a system of linear equations to check whether there
+    exists a candidate for a Nash equilibrium for the given supports.
+
+    The reference implementation uses `scipy.optimize.linprog`
+    with the default solver -- 'highs'. You can find more information at
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linprog.html
+
+    Parameters
+    ----------
+    matrix : np.ndarray
+        A payoff matrix of one of the players
+    row_support : np.ndarray
+        The row player's support
+    col_support : np.ndarray
+        The column player's support
+
+    Returns
+    -------
+    np.ndarray | None
+        The opponent's strategy, if it exists, otherwise `None`
+    """
+
+    raise NotImplementedError
+
+
+def support_enumeration(
+    row_matrix: np.ndarray, col_matrix: np.ndarray
+) -> list[tuple[np.ndarray, np.ndarray]]:
+    """Run the Support Enumeration algorithm and return a list of all Nash equilibria
+
+    Parameters
+    ----------
+    row_matrix : np.ndarray
+        The row player's payoff matrix
+    col_matrix : np.ndarray
+        The column player's payoff matrix
+
+    Returns
+    -------
+    list[tuple[np.ndarray, np.ndarray]]
+        A list of strategy profiles corresponding to found Nash equilibria
+    """
+
+    raise NotImplementedError
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
