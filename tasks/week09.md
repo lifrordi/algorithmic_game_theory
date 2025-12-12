3219c671509d2d05d3aab93e260025eecebaf45e
Radovan Haluska <radovan.haluska1@gmail.com>
2025-12-12 12:08:05 +0100

    Publish homework for Week 9

---
+++ tasks/week09.md
@@ -0,0 +1,11 @@
+# Week 9 Homework
+
+## 1) Counterfactual Regret Minimization
+- Implement Counterfactual Regret Minimization and test it on Kuhn Poker
+
+## 2) CFR+
+- Implement CFR+ and compare its performance to standard CFR on Kuhn Poker
+
+# Week 9 Template
+
+See `templates/week09.py` for function definitions and docstrings describing expected functionality.
---
+++ templates/week09.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def cfr(*args, **kwargs):
+    """Run the CFR algorithm for a given number of iterations."""
+
+    raise NotImplementedError
+
+
+def cfr_plus(*args, **kwargs):
+    """Run the CFR+ algorithm for a given number of iterations."""
+
+    raise NotImplementedError
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
