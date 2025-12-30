ed0cb3db4f9f2e0120ec7c48f56afd54275ae1c3
Radovan Haluska <radovan.haluska1@gmail.com>
2025-12-30 11:15:00 +0100

    Publish homework for Week 10

---
+++ tasks/week10.md
@@ -0,0 +1,11 @@
+# Week 10 Homework
+
+## 1) Discounted CFR
+- Implement Discounted CFR and test it on Kuhn Poker
+
+## 2) Monte Carlo CFR
+- Implement Monte Carlo CFR and test it on Kuhn Poker
+
+# Week 10 Template
+
+See `templates/week10.py` for function definitions and docstrings describing expected functionality.
---
+++ templates/week10.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def discounted_cfr(*args, **kwargs):
+    """Run the Discounted CFR algorithm for a given number of iterations."""
+
+    raise NotImplementedError
+
+
+def monte_carlo_cfr(*args, **kwargs):
+    """Run the Monte Carlo CFR algorithm for a given number of iterations."""
+
+    raise NotImplementedError
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
